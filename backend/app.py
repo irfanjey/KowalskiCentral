e07e22d98ea7f7efd4245fda46e4cac7187131fa
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-08-03 16:14:54 -0400

    help me

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 import requests
-from bs4 import BeautifulSoup
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"]  = "mongodb+srv://g:g@terrahacks.sv2utes.mongodb.net/?retryWrites=true&w=majority&appName=Terrahacks"
+mongo = PyMongo(app)
 
 @app.route("/")
 def home():
-    return "<p>Hello world</p>"
+    online_users = mongo.db.users.find({"online": True})
+    return online_users
 
 @app.route("/login")
 def login():
