e9ada1e8040bfbc8c8e8100446e1e82486f19d0f
irfanjey <ijeyavudeen@gmail.com>
2024-08-03 18:56:01 -0400

    front-end changes 4

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../Assets/kowalski_logo.png';
 import './Navbar.css';
 
 const Navbar = () => {
   //const [menu, setMenu] = useState("home");
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="navbar">
         <div className="nav-logo">
@@ -12,6 +14,16 @@ const Navbar = () => {
               <p>Kowalski</p>
               <p>Central</p>
             </div>
+        </div>
+        <div className='nav_login'>
+          {loggedIn 
+            ? 
+            <button>Logout</button>
+            :
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          }
         </div>
     </div>
   )
